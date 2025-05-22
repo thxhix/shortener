a967a8a8e1c4259c65f5f753fac5e649c6ab4763
Maxim <tika4ev55@gmail.com>
2025-05-22 13:53:22 +0300

    – Убрал DROP таблицы, переписал на SQL проверку поля

--- internal/database/migrations/Links.go
+++ internal/database/migrations/Links.go
@@ -5,20 +5,27 @@ import (
 )
 
 func Migrate(db interfaces.Database) error {
-	// Дропать не надо, понятное дело, но для простоты пусть будет так.
-	// Надеюсь позднее будет время, разберсь с норм миграциями..
 	query := `
-	DROP TABLE IF EXISTS shortener;
-
-	CREATE TABLE IF NOT EXISTS shortener (
-		id SERIAL PRIMARY KEY,
-		original VARCHAR(512) UNIQUE NOT NULL,
-		shorten VARCHAR(10) UNIQUE NOT NULL,
-		user_id UUID NULL,
-		created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP 
-	);
-
-	CREATE INDEX IF NOT EXISTS idx_shortener_user_id ON shortener(user_id);`
+		CREATE TABLE IF NOT EXISTS shortener (
+			id SERIAL PRIMARY KEY,
+			original VARCHAR(512) UNIQUE NOT NULL,
+			shorten VARCHAR(10) UNIQUE NOT NULL,
+			created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+		);
+		
+		DO $$
+		BEGIN
+			IF NOT EXISTS (
+				SELECT 1
+				FROM information_schema.columns 
+				WHERE table_name='shortener' AND column_name='user_id'
+			) THEN
+				ALTER TABLE shortener ADD COLUMN user_id UUID NULL;
+			END IF;
+		END$$;
+		
+		CREATE INDEX IF NOT EXISTS idx_shortener_user_id ON shortener(user_id);
+	`
 	driver := db.GetDriver()
 	if driver == nil {
 		return nil
